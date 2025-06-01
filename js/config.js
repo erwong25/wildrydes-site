66807b2e80e8784d563cbf7cbe63616ca89d0f5d
erwong25 <111260585+erwong25@users.noreply.github.com>
2025-06-01 15:46:31 -0700

    No client secret user pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_vhcQqV8Yy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '10f8oc5f5q77j7fsk0d0mvvrrl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5kg2jdtljs3clcjithc1b0u5sb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
+        
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
